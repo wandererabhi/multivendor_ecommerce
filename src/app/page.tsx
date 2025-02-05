f9b5b14724b5a2985b74b75c903bc9514e6d70d3
abhilash Mailugundla <abhilash5611@outlook.com>
2025-02-05 18:16:11 +0530

    basic setup till the theme setp like dark and light

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import ThemeToggle from "@/components/shared/theme-toggle";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>Hello World</div>
+    <div className="p-5">
+      <div className="w-100 flex justify-end">
+        <ThemeToggle />
+      </div>
+
+      <h1 className=" text-blue-500 font-barlow">Hello World!</h1>
+      <Button variant="outline">Click here</Button>
+    </div>
   );
 }
